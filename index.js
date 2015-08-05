9281294009db0ac5e82eef18b4b79e93a650e58b
Damon Toal-Rossi <damon@uber.com>
2015-08-05 09:40:25 -0600

    Comments for clarity

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+// Import Express
 var express = require('express');
 
+// Implement a new Application
 var app = express();
 
+// Application route listenters
 app.get('/', function rootPath(req, res) {
   res.send('Hello World!');
 });
 
+// Star the server
 var server = app.listen(3000, function serveApp() {
   var host = server.address().host;
   var port = server.address().port;
